5ba816264e2f6e6daa89e1ecf2c1e3bb98ea0788
Gaston Senac <41843454+gaston-senac@users.noreply.github.com>
2026-04-08 21:15:45 +1200

    new model for job and new seed

--- models/job.ts
+++ models/job.ts
@@ -1,13 +1,28 @@
 export type Priority = 'High' | 'Medium' | 'Low'
-export type JobStatus = 'Todo' | 'In Progress' | 'Review' | 'Done'
+export type JobStatus = 'New' | 'Quoted' | 'Awaiting Inspection' | 'Done'
 
 export interface Job {
   id: number
+  customerId: number
   title: string
-  client: string
-  location: string
-  priority: Priority
+  problem?: string
+  // location: string
+  // priority: Priority
   status: JobStatus
-  dueDate: string
-  description?: string
+  quote: number
+  startDate: string
 }
+
+// {
+//   id: 1,
+//   tradie_id: 1,
+//   customer_id: 1,
+//   status: 'Awaiting inspection',
+//   title: 'Broken Faucet',
+//   problem: 'Customer states that the faucet is broken? Inspection required',
+//   inspection: 'Found valve to be siezed, replacment ordered.',
+//   quote: 85,
+//   notes: '',
+//   start_date: '2026-04-08',
+//   end_date: '2026-04-12',
+// }
