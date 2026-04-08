3e8e2a90c18c33fe4d739df4fb17e06084c823b4
Austin Dinh <austin@dinh.nz>
2026-04-08 16:26:31 +1200

    resolve conflict

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,16 +1,11 @@
 import { Outlet } from 'react-router'
-import Header from './Header.tsx'
-
-export default function Layout() {
-  return (
-    <>
-      <Header />
-      <main>
+import Header from './Header'
 import NavHeader from './NavHeader'
 
 export default function Layout() {
   return (
     <div className="flex min-h-screen flex-col bg-zinc-950 font-sans text-zinc-100">
+      <Header />
       <NavHeader />
       <main className="flex-1 overflow-hidden">
         <Outlet />
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,12 +1,7 @@
 import { createRoutesFromElements, Route } from 'react-router'
 import Layout from './components/Layout.tsx'
 import App from './components/App.tsx'
-
-export default createRoutesFromElements(
-  <Route element={<Layout />}>
-    <Route index element={<App />} />
 import KanbanBoard from './components/KanbanBoard.tsx'
-import Layout from './components/Layout.tsx'
 
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
