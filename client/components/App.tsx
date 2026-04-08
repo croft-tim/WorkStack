5e23bba7012fca78af80bb1b05e63a3d4d7879f4
Jarad Brown <jaraddotbrown@gmail.com>
2026-04-08 15:39:46 +1200

    job-component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,5 @@
 import { useFruits } from '../hooks/useFruits.ts'
+import JobSummary from './Job.tsx'
 
 function App() {
   const { data } = useFruits()
@@ -9,6 +10,7 @@ function App() {
         <h1 className="text-3xl font-bold underline">
           Fullstack Boilerplate - with Fruits!
         </h1>
+        <JobSummary />
         <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
       </div>
     </>
---
+++ client/components/Job.tsx
@@ -0,0 +1,34 @@
+type JobSummaryProps = {
+  title: string
+  dueDate: string
+  status: string
+}
+
+export default function JobSummary({
+  title,
+  dueDate,
+  status,
+}: JobSummaryProps) {
+  return (
+    <div className="mx-auto flex max-w-4xl items-center justify-between gap-6 rounded-md bg-zinc-100 p-4">
+      <div className="flex min-w-[180px] items-center gap-4">
+        <h2 className="text-3xl font-medium">{title}</h2>
+        <p className="text-lg text-zinc-700">Due: {dueDate}</p>
+      </div>
+
+      <div className="flex min-w-[260px] flex-col items-end gap-3">
+        <p className="text-lg text-zinc-800">Status : {status}</p>
+
+        <div className="flex gap-4">
+          <button className="rounded-md border border-zinc-700 bg-white px-4 py-2 text-lg shadow-sm transition hover:bg-zinc-50">
+            Change Status
+          </button>
+
+          <button className="rounded-md border border-zinc-700 bg-white px-6 py-2 text-lg shadow-sm transition hover:bg-zinc-50">
+            Open
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
