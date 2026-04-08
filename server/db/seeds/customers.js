b9213bcda0327a45d6abe443174c529d410bc2de
Gaston Senac <41843454+gaston-senac@users.noreply.github.com>
2026-04-08 20:56:36 +1200

    seeds for customers with faker

--- server/db/seeds/customers.js
+++ server/db/seeds/customers.js
@@ -1,17 +1,34 @@
+import { faker } from '@faker-js/faker'
+
 export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('customers').del()
 
   // Inserts seed entries
-  await knex('customers').insert([
-    {
-      id: 1,
-      name: 'Jessie',
-      address: '69 place over there',
-      phone: '0210231212',
-      email: 'real@beanmail.com',
+  // await knex('customers').insert([
+  //   {
+  //     id: 1,
+  //     name: 'Jessie',
+  //     address: '69 place over there',
+  //     phone: '0210231212',
+  //     email: 'real@beanmail.com',
+  //     notes: '',
+  //     rating: 7.5,
+  //   },
+  // ])
+
+  // Insert seeds
+  const people = Array.from({ length: 15 }).map((_, i) => {
+    const name = faker.person.fullName()
+    return {
+      id: i,
+      name: name,
+      address: faker.location.streetAddress(),
+      phone: faker.phone.number({ style: 'international' }),
+      email: faker.internet.email({ firstName: name }),
       notes: '',
-      rating: 7.5,
-    },
-  ])
+      rating: faker.number.float({ multipleOf: 0.25, min: 0, max: 10 }),
+    }
+  })
+  await knex('customers').insert(people)
 }
