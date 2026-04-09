6fb8f723f3b77ca112ac4aff2e6d226f1ac1239f
Austin Dinh <austin@dinh.nz>
2026-04-09 13:04:38 +1200

    create API client function to add job

--- client/components/NewJobPage.tsx
+++ client/components/NewJobPage.tsx
@@ -1,13 +1,14 @@
 import { useNavigate } from 'react-router'
 import JobForm from '../components/JobForm'
 import { JobData } from '../../models/job'
+import { addJob } from '../apis/jobs'
 
 export default function NewJobPage() {
   const navigate = useNavigate()
 
   async function handleSubmit(data: JobData) {
-    await createJob(data) // your API call here
-    navigate('/jobs')
+    const id = await addJob(data) // your API call here
+    navigate(`/jobs/${id}`)
   }
 
   return (
