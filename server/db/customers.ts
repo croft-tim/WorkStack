77b23130ff5286cf0923a8891478ec8e641ef6de
Jullian Bublitz <jullianbublitz.garratt@gmail.com>
2026-04-09 15:20:31 +1200

    Added customer endpoints

--- server/db/customers.ts
+++ server/db/customers.ts
@@ -1,7 +1,12 @@
 import db from './connection.ts'
-import { Customer, CustomerData } from '../../models/customer.ts'
+import { Customer } from '../../models/customer.ts'
 
 export async function getCustomerById(id: number | string) {
-  const customer = await db('customers').select().first().where({id})
+  const customer = await db('customers').select().first().where({ id })
   return customer as Customer
+}
+
+export async function getCustomers() {
+  const customer = await db('customers').select()
+  return customer as Customer[]
 }
---
+++ server/routes/customers.ts
@@ -0,0 +1,108 @@
+import { Router } from 'express'
+// import checkJwt, { JwtRequest } from '../auth0.ts'
+// import { StatusCodes } from 'http-status-codes'
+
+import * as db from '../db/customers.ts'
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const jobs = await db.getCustomers()
+    res.json(jobs)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+
+    if (Number.isNaN(id)) {
+      res.status(400).json({ message: 'Invalid job id' })
+      return
+    }
+
+    const job = await db.getCustomerById(id)
+
+    if (job == null) {
+      res.status(404).json({ message: 'Job not found' })
+      return
+    } else {
+      res.json(job)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newJob = req.body
+    const job = await db.addCustomer(newJob)
+    res.json(job)
+    // .setHeader('Location', `${req.baseUrl}/${id}`)
+    // .sendStatus(StatusCodes.CREATED)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+
+    if (Number.isNaN(id)) {
+      res.status(400).json({ message: 'Invalid job id' })
+      return
+    }
+
+    const updatedJobData = req.body
+    const updatedJob = await db.updateCustomerById(id, updatedJobData)
+    res.json(updatedJob)
+    // .setHeader('Location', `${req.baseUrl}/${id}`)
+    // .sendStatus(StatusCodes.NO_CONTENT)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+
+    if (Number.isNaN(id)) {
+      res.status(400).json({ message: 'Invalid job id' })
+      return
+    }
+
+    await db.deleteCustomerById(id)
+    res.sendStatus(200)
+    // .setHeader('Location', `${req.baseUrl}/${id}`)
+    // .sendStatus(StatusCodes.NO_CONTENT)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//Do later
+// router.post('/', checkJwt, async (req: JwtRequest, res, next) => {
+//   if (!req.auth?.sub) {
+//     res.sendStatus(StatusCodes.UNAUTHORIZED)
+//     return
+//   }
+
+//   try {
+//     const { owner, name } = req.body
+//     const id = await db.addFruit({ owner, name })
+//     res
+//       .setHeader('Location', `${req.baseUrl}/${id}`)
+//       .sendStatus(StatusCodes.CREATED)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
+
+export default router
