c47cf11241431a3ca5e16537871565ce90a80eaa
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-04-13 13:52:03 +1200

    feat: change job status on kanban

--- client/components/KanbanCard.tsx
+++ client/components/KanbanCard.tsx
@@ -1,6 +1,7 @@
-import { Job } from '../../models/job'
+import { Job, JobStatus, statuses } from '../../models/job'
 import { useNavigate } from 'react-router'
 import { useDeleteJob } from '../hooks/useDeleteJob'
+import { useUpdateJob } from '../hooks/useJob'
 
 interface Props {
   job: Job
@@ -8,6 +9,7 @@ interface Props {
 
 export default function KanbanCard({ job }: Props) {
   const { mutate: deleteJob } = useDeleteJob()
+  const { mutate: updateJob } = useUpdateJob()
   const navigate = useNavigate()
 
   const priorityColors = {
@@ -16,10 +18,19 @@ export default function KanbanCard({ job }: Props) {
     Low: 'bg-emerald-500/10 text-emerald-500 border-emerald-500/20',
   }
 
+  const handleStatusChange = (nextStatus: JobStatus) => {
+    updateJob({
+      ...job,
+      status: nextStatus,
+    })
+  }
+
   return (
     <div
       onClick={() => navigate(`/jobs/${job.id}`)}
-      className="group relative flex cursor-pointer flex-col gap-3 rounded-lg border transition-all duration-200 border-slate-200 bg-white dark:border-zinc-800 dark:bg-zinc-800/50 pink:border-pink-200 pink:bg-pink-50 p-4 hover:shadow-lg hover:shadow-black/5 dark:hover:border-zinc-700 dark:hover:shadow-black/20 pink:hover:border-pink-300 pink:hover:shadow-pink-500/10"
+      className="pink:border-pink-200 pink:bg-pink-50 pink:hover:border-pink-300 pink:hover:shadow-pink-500/10 group relative flex cursor-pointer flex-col gap-3 rounded-lg border border-slate-200
+  bg-white p-4 transition-all duration-200 hover:shadow-lg hover:shadow-black/5
+  dark:border-zinc-800 dark:bg-zinc-800/50 dark:hover:border-zinc-700 dark:hover:shadow-black/20"
     >
       <div className="flex items-start justify-between">
         {/* <span className="text-xs font-medium text-zinc-500">{job.id}</span> */}
@@ -31,16 +42,16 @@ export default function KanbanCard({ job }: Props) {
       </div>
 
       <div>
-        <h3 className="text-sm font-semibold transition-colors text-slate-800 dark:text-zinc-100 pink:text-pink-900 group-hover:text-amber-500 pink:group-hover:text-pink-600">
+        <h3 className="pink:text-pink-900 pink:group-hover:text-pink-600 text-sm font-semibold text-slate-800 transition-colors group-hover:text-amber-500 dark:text-zinc-100">
           {job.title}
         </h3>
-        <p className="mt-1 text-xs text-slate-500 dark:text-zinc-400 pink:text-pink-700">
+        <p className="pink:text-pink-700 mt-1 text-xs text-slate-500 dark:text-zinc-400">
           {job.client}
         </p>
       </div>
 
-      <div className="mt-2 flex flex-col gap-2 border-t pt-3 border-slate-100 dark:border-zinc-800 pink:border-pink-100">
-        <div className="flex items-center gap-2 text-[11px] text-slate-400 dark:text-zinc-500 pink:text-pink-500">
+      <div className="pink:border-pink-100 mt-2 flex flex-col gap-2 border-t border-slate-100 pt-3 dark:border-zinc-800">
+        <div className="pink:text-pink-500 flex items-center gap-2 text-[11px] text-slate-400 dark:text-zinc-500">
           <svg
             className="h-3.5 w-3.5"
             fill="none"
@@ -63,7 +74,7 @@ export default function KanbanCard({ job }: Props) {
           {job.address}
         </div>
 
-        <div className="flex items-center gap-2 text-[11px] text-slate-400 dark:text-zinc-500 pink:text-pink-500">
+        <div className="pink:text-pink-500 flex items-center gap-2 text-[11px] text-slate-400 dark:text-zinc-500">
           <svg
             className="h-3.5 w-3.5"
             fill="none"
@@ -82,9 +93,25 @@ export default function KanbanCard({ job }: Props) {
       </div>
 
       <div className="mt-2 flex gap-2">
-        <button className="rounded-md border border-slate-200 bg-slate-50 px-3 py-1.5 text-xs text-slate-600 transition hover:bg-slate-100 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-200 dark:hover:bg-zinc-700 pink:border-pink-200 pink:bg-pink-100 pink:text-pink-700 pink:hover:bg-pink-200">
-          Change Status
-        </button>
+        <div onClick={(e) => e.stopPropagation()}>
+          <select
+            value={job.status}
+            onClick={(e) => e.stopPropagation()}
+            onChange={(e) => {
+              e.stopPropagation()
+              handleStatusChange(e.target.value as JobStatus)
+            }}
+            className="pink:border-pink-200 pink:bg-pink-100 pink:text-pink-700 pink:hover:bg-pink-200 dark:hover:bg- zinc-700 rounded-md border
+  border-slate-200 bg-slate-50 px-3 py-1.5 text-xs text-slate-600 transition hover:bg-slate-100 dark:border-zinc-700 dark:bg-zinc-800
+  dark:text-zinc-200"
+          >
+            {statuses.map((status) => (
+              <option key={status} value={status}>
+                {status}
+              </option>
+            ))}
+          </select>
+        </div>
 
         <button
           onClick={(e) => {
@@ -93,7 +120,7 @@ export default function KanbanCard({ job }: Props) {
               deleteJob(job.id)
             }
           }}
-          className="rounded-md border border-rose-500/30 bg-rose-500/10 px-3 py-1.5 text-xs text-rose-400 transition hover:bg-rose-500/20 pink:border-rose-300 pink:bg-rose-50 pink:text-rose-600 pink:hover:bg-rose-100"
+          className="pink:border-rose-300 pink:bg-rose-50 pink:text-rose-600 pink:hover:bg-rose-100 rounded-md border border-rose-500/30 bg-rose-500/10 px-3 py-1.5 text-xs text-rose-400 transition hover:bg-rose-500/20"
         >
           Delete
         </button>
